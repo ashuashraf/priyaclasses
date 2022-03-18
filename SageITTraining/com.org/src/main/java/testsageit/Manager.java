06278c53a61413e8f9a5c0986837be41253eb548
ashuashraf <ashuashraf694@gmail.com>
2022-03-18 15:47:35 +0530

    CoreJava

---
+++ SageITTraining/com.org/src/main/java/testsageit/Manager.java
@@ -0,0 +1,24 @@
+package testsageit;
+public class Manager extends Employee {
+
+    private int salary;
+    private String grade;
+
+    public void setSalary(int salary) {
+        salary = this.salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setGrade(String grade) {
+        grade = this.grade;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+ 
+}
