1cf7d20f0e214f89a9cbc8973c324e5b16c75fab
ashuashraf <ashuashraf694@gmail.com>
2022-03-30 16:33:34 +0530

    Project Completed

---
+++ employee-management-system/src/main/java/com/org/employee/model/Employee.java
@@ -0,0 +1,107 @@
+package com.org.employee.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name ="day_of_birth")
+	private String dateofbirth;
+
+	@Column(name ="phone_number")
+	private String phonenumber;
+
+	@Column(name ="gender")
+	private String gender ;
+
+	@Column(name ="experience")
+	private String experience ;
+
+	@Column(name ="marital_status")
+	private String maritalstatus ;
+
+	@Column(name ="qualification ")
+	private String qualification  ;
+
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	public String getMaritalstatus() {
+		return maritalstatus;
+	}
+	public void setMaritalstatus(String maritalstatus) {
+		this.maritalstatus = maritalstatus;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public String getDateofbirth() {
+		return dateofbirth;
+	}
+	public void setDateofbirth(String dateofbirth) {
+		this.dateofbirth = dateofbirth;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
